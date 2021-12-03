4e7ad462ffeb12bdf8d8284432405070661f49cb
edgaramv <95459235+edgaramv@users.noreply.github.com>
2021-12-02 23:11:22 -0600

    Add files via upload

---
+++ instalacion_modulos.py
@@ -0,0 +1,17 @@
+import os
+
+
+Python = os.system("sudo apt-get install python3")
+
+pip1 = os.system("sudo apt-get install python3-pip")
+
+flask = os.system("sudo pip install flask")
+
+quiracdroom = os.system("sudo sed -i '/cdrom/d' /etc/apt/sources.list")
+
+connector1 = os.system("sudo pip install mysql-connector")
+
+connector2 = os.system("sudo pip install mysql-connector-python")
+
+
+
